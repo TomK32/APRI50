c037d5b24d4bfe3c6716565de8473c178821d55e
Thomas R. Koll <tomk32@tomk32.de>
2013-12-22 21:49:13 +0100

    Simplify the noise shader and make it more blocky

--- shader/noise.lua
+++ shader/noise.lua
@@ -1,21 +1,18 @@
 return love.graphics.newShader[[
 extern float factor = 0.2;
 extern float addPercent = 0.05;
-extern float clamp = 0.99;
 
 // from http://www.ozone3d.net/blogs/lab/20110427/glsl-random-generator/
 float rand(vec2 n)
 {
-  return 0.5 + 0.5 * fract(sin(dot(n.xy, vec2(12.9898, 78.233))) * 43758.5453);
+  return 0.5 * fract(sin(dot(vec2(floor(n.x*400), floor(n.y*400)), vec2(12.9898, 78.233))) * 43758.5453);
 }
 
-vec4 effect(vec4 color, Image tex, vec2 tc, vec2 sc)
+vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords)
 {
-  float grey = 1 * rand(tc * factor);
-  float clampedGrey = max(grey, clamp);
+  float grey = 1 - 0.2 * rand(texture_coords);
   vec4 noise = vec4(grey, grey, grey, 1);
-  vec4 clampedNoise = vec4(clampedGrey, clampedGrey, clampedGrey, 1);
-  return (Texel(tex, tc) * clampedNoise * (1 - addPercent) + noise * addPercent) * color;
+  return (Texel(texture, texture_coords) * noise) * color;
 }
 ]]
 
