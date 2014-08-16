0cfcda3028046a1312cf8f4d2757f3f5db633cee
Thomas R. Koll <tomk32@tomk32.de>
2014-08-16 19:29:44 +0200

    Change Quickie button colours to something blue-ish

--- lib/quickie/style-default.lua
+++ lib/quickie/style-default.lua
@@ -29,9 +29,9 @@ local utf8 = require(BASE .. 'utf8')
 
 -- default style
 local color = {
-	normal = {bg = {78,78,78}, fg = {200,200,200}, border={20,20,20}},
-	hot    = {bg = {98,98,98}, fg = {69,201,84},   border={30,30,30}},
-	active = {bg = {88,88,88}, fg = {49,181,64},   border={10,10,10}}
+	normal = {bg = {0,72,92}, fg = {200,200,200}, border={20,20,20}},
+	hot    = {bg = {20,72,92}, fg = {69,201,84},   border={30,30,30}},
+	active = {bg = {20,72,92}, fg = {49,181,64},   border={10,10,10}}
 }
 
 -- box drawing
